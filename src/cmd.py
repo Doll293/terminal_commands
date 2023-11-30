d8e17d338f66be71e2ace91c5f54758e7fd72a2b
Dan-Popescu <popescu_dan@outlook.com>
2023-11-30 16:52:26 +0100

    Rename main to cmd and fix path bug

--- src/cmd.py
+++ src/cmd.py
@@ -1,8 +1,9 @@
-from src.commands.ls.ls import ls
+from commands.ls.ls import ls
 
 while True:
     command = input()
 
     if command == "ls":
         ls()
-
+    elif command != "ls":
+        exit()
--- src/commands/ls/ls.py
+++ src/commands/ls/ls.py
@@ -2,11 +2,9 @@ import os
 
 
 def ls():
-    all_files_and_dir = []
-    all_files_and_dir.append(os.curdir)
-    all_files_and_dir.append(os.pardir)
-    for dir in os.listdir():
-        all_files_and_dir.append(dir)
+    all_files_and_dir = [os.curdir, os.pardir]
+    for dir_item in os.listdir():
+        all_files_and_dir.append(dir_item)
 
     for el in all_files_and_dir:
         print(el)
